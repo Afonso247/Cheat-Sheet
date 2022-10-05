a5ae9eef297b409e8b7110be35ff2f956ef9984e
Afonso Henrique <97695756+Afonso247@users.noreply.github.com>
2022-10-04 23:10:22 -0300

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,8 @@
     <p>Clique em <a href="/projetos">Meus projetos</a> para acessar alguns dos meus 
        principais projetos publicados no meu <a href="https://github.com/Afonso247" 
        target="_blank">Github</a>.</p>
-    <p>Além disso, acesse o <a href="/sobre">Sobre mim</a> e saiba mais sobre a mim, as  
-       minhas Habilidades, Experiências, e etc.
+    <p>Além disso, acesse o <a href="/sobre">Sobre mim</a> e saiba mais sobre mim, as  
+       minhas <strong>habilidades</strong>, <strong>experiências</strong> e etc.
     </p>
     <p class="thing"><strong>Vá para os links do canto superior direito e começe a navegar! ^-^</strong></p>
     <!-- <small>Ou, pelo menos, o rascunho disso...</small> -->
